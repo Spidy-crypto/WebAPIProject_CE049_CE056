d477acf145456808c97e21320a2a47331be09e87
Spidy-crypto <rajkalathiya699@gmail.com>
2021-03-31 16:44:57 +0530

    Add place

--- TourManagementApi/Controllers/TourController.cs
+++ TourManagementApi/Controllers/TourController.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TourManagementApi.Models;
 
 namespace TourManagementApi.Controllers
 {
     public class TourController : ApiController
     {
+        SqlConnection con = null;
+        SqlCommand cmd = null;
+
+        String ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename = C:\USERS\RAJKA\ONEDRIVE\DESKTOP\WEBAPIPROJECT_CE049_CE056\CLIENT\APP_DATA\DATABASE1.MDF;Integrated Security = True";
+
+
         // GET: api/Tour
         public IEnumerable<string> Get()
         {
@@ -22,8 +30,47 @@ namespace TourManagementApi.Controllers
         }
 
         // POST: api/Tour
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post(Tour t)
         {
+            try
+            {
+                con = new SqlConnection();
+                con.ConnectionString = this.ConnectionString;
+                using (con)
+                {
+                    string command = "INSERT INTO [Place](name,description,price,imagepath)VALUES(@name,@description,@price,@imagepath)";
+                    cmd = new SqlCommand(command, con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@name", t.name);
+                    cmd.Parameters.AddWithValue("@description", t.desc);
+                    cmd.Parameters.AddWithValue("@price", t.price);
+                    cmd.Parameters.AddWithValue("@imagepath", t.imagepath);
+                    int res = cmd.ExecuteNonQuery();
+                    if (res == 1)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, "Tour added"); ;
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.BadRequest, "");
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "");
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Dispose();
+                }
+                if (cmd != null)
+                {
+                    cmd.Dispose();
+                }
+            }
         }
 
         // PUT: api/Tour/5
